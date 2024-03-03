0273b7603174c1830d1540e0542c3f58d122f8db
Firda Rosiana T <firdaroziana10@gmail.com>
2024-03-03 20:28:37 +0700

    blog homePages for user

---
+++ BloggieWeb/Controllers/BlogsController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BloggieWeb.Controllers
+{
+    public class BlogsController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index(string urlHandle)
+        {
+            return View();
+        }
+    }
+}
